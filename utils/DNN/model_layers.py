ea8420cc3948ca77c677c711c9498ccf40b22327
Fynn Martin Gilbert <fynn.martin.gilbert@gmail.com>
2023-10-18 16:54:07 +0200

    outsource Encoder and update gitignore

---
+++ utils/DNN/model_layers.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization
+from tensorflow.keras.layers import Add, Dense, Layer
+
+
+class TransformerEncoder(Layer):
+    def __init__(
+        self,
+        units: int,
+        num_heads: int,
+        key_dim: int,
+        idx: int,
+        activation: str = "relu",
+        dropout: float=0.05,
+        use_bias: bool=False,
+        use_PreLN: bool=False,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.self_attention_layer = MultiHeadAttention(
+            num_heads=num_heads, key_dim=key_dim,
+            dropout=dropout,               # Hyperparameter
+            use_bias=use_bias,                     # usually False, but technically Hyperparameter
+            name=f"Encoder-SelfAttentionLayer-{idx}"
+        )
+        
+        self.add1 = Add(name=f"Encoder-1st-AdditionLayer-{idx}")
+        self.add2 = Add(name=f"Encoder-2nd-AdditionLayer-{idx}")
+
+        self.layernorm1 = LayerNormalization(name=f"Encoder-1st-NormalizationLayer-{idx}")
+        self.layernorm2 = LayerNormalization(name=f"Encoder-2nd-NormalizationLayer-{idx}")
+
+        self.feed_forward_layer = Dense(
+            units=units,
+            activation=activation,
+            name=f"Encoder-FeedForwardLayer_{idx}"
+        )
+
+        # Actual Parameters:
+        self.use_PreLN = use_PreLN
+
+    def call(self, inputs):
+        """
+        Check the paper 'On Layer Normalization in the Transformer Architecture'
+        """
+
+        #### Attention Block ####
+        
+        attention_inputs = inputs
+
+        if self.use_PreLN:
+            attention_inputs = self.layernorm1(attention_inputs)
+
+        self_attention_output = self.self_attention_layer(
+            query=attention_inputs,
+            value=attention_inputs,
+            key = attention_inputs,
+            use_causal_mask=False, # only makes sense for Time series / causal flow data
+        )
+
+        inputs = self.add1([inputs, self_attention_output])
+
+        if not self.use_PreLN:
+            inputs = self.layernorm1(inputs)
+
+        #### Feed Forward Block ####
+
+        ff_inputs = inputs
+
+        if self.use_PreLN:
+            ff_inputs = self.layernorm1(ff_inputs)
+
+        feed_forward_output = self.feed_forward_layer(ff_inputs)
+
+        inputs = self.add2([inputs, feed_forward_output])
+
+        if not self.use_PreLN:
+            inputs = self.layernorm2(inputs)
+
+        return inputs
