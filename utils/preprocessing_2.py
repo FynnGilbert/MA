ba06bd30a10a0bffe43e0c34130e408452d4ea20
Fynn Martin Gilbert <fynn.martin.gilbert@gmail.com>
2023-11-30 11:08:12 +0100

    Option for seperate stop indicies as model input

---
+++ utils/preprocessing_2.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+import polars as pl
+import numpy as np
+
+def extract_raw_data(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Extracts features via regex from raw txt-file input.
+    Drops rows which dont contain a truck_id or 3D packing instances
+    """
+    df = df.with_columns([
+        pl.Series(name="index", values=np.arange(len(df.collect()))),
+        #pl.Series(name="index", values=np.arange(len(df))),
+        pl.col("raw").str.extract("in Truck (\w\d{9})\n").alias("truck_id"),
+        pl.col("raw").str.extract("dataset: (\w*)\n").alias("dataset"),
+        pl.col("raw").str.extract("instance: ([A-Z0-9]*)\n").alias("instance"),
+    ])
+    
+    return df
+
+
+def explode_instances_into_stacks(df: pl.LazyFrame) -> pl.DataFrame:
+    """
+    Use regex to extract all stacks made of item ids, and explode df
+    into rows of stacks, belonging to an instance.
+    """
+    
+    df = (
+        df.with_columns([
+            pl.col("raw").str.extract_all("Stack \d* with items: (\[.*\])\n").alias("stacks")
+        ])
+          .drop(["raw"])
+          .explode(columns = ["stacks"])\
+          .with_columns([
+              pl.col("stacks").str.extract("Stack (\d*) with").cast(pl.Int64, strict=False).alias("stack_id"),
+              pl.col("stacks").str.extract_all("(\d{10}_\d{14})").alias("item_id")
+          ])
+    )
+    return df
+
+
+def explode_stacks_into_items(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Use regex to identify a stack and explode it into items,
+    belonging to a stack, which again belongs to an instance.
+    """
+    df = df.with_columns([
+        pl.col("stacks").str.extract("Stack (\d*) with").cast(pl.Int64, strict=False).alias("stack_id"),
+        pl.col("stacks").str.extract_all("(\d{10}_\d{14})").alias("item_id")
+    ]).drop("stacks").explode("item_id")\
+    
+    return df
+
+
+def join_items(df: pl.DataFrame, items) -> pl.DataFrame:
+    """
+    Perform a simple left join to add item level info.
+    
+    Validation sadly not supported for multiple keys (yet?)
+    """
+    
+    return df.join(items, how = "left", on = ["dataset", "instance", "item_id"])
+
+
+def group_items_by_stack(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Group item level into back into a stack
+    
+    - Note that the Width and Length are the same for all items
+    - Weight and Height have to be summed up, correcting for nesting height
+    - forcedOrientation of a single item makes the whole stack oriented
+    - Include Logistics info for stop differentation down the road
+    
+    """
+    
+    df = df.group_by(["index", "dataset", "instance", "truck_id", "stack_id"],
+                    maintain_order = True)\
+    .agg([
+        #pl.count("item_id").alias("items"),
+        pl.max("Length"),
+        pl.max("Width"),
+        #pl.sum("NestedHeight"),
+        #pl.last("Nesting height"),
+        pl.sum("Weight"),
+        pl.any("ForcedLength").cast(pl.Float32),
+        pl.any("ForcedWidth").cast(pl.Float32),
+        # Logistic order info
+        pl.first("Supplier code"),
+        pl.first("Supplier dock"),
+        pl.first("Plant dock"),
+    ]).sort(["index", "stack_id"]).drop(["stack_id"])
+
+    return df
+
+
+def join_truck_loading_order(df: pl.DataFrame, truck_stops) -> pl.DataFrame:
+    """
+    Extract the loading orders in the truck_stops
+    from the supplier and plant order, given by the stacks/items
+    
+    TODO:
+    -----
+    
+    - Include a check whether the last item in a stack is different from the first
+        This would mean that it is a multistack, making the problem much more difficult
+        Jakob said this is often explicitly forbidden by the heuristic, and rarely the 
+        case anyways.
+    """
+    
+    truck_join_clms = ["truck_id", "dataset", "instance",
+                       "Supplier code", "Supplier dock", "Plant dock"]
+    
+    df = df.join(truck_stops, how = "left", on = truck_join_clms )
+    
+    df = df.drop(truck_join_clms[-3:])
+    
+    return df
+    
+
+def append_truck_info(df: pl.DataFrame, truck_dims: pl.DataFrame) -> pl.DataFrame:
+    """
+    Add trucks into dataset adding them as rows
+    with exactly the same features as the original df
+    """
+
+    # join to get instance id
+    truck_dims = (
+        truck_dims
+        .join(df, on = ["dataset", "instance", "truck_id"], how="inner")
+        .unique()
+    )
+    
+    df = df.drop(["dataset", "instance", "truck_id"])
+    
+    truck_dims = truck_dims.with_columns([
+        pl.col("EMmm").cast(pl.Float32).alias("ForcedLength"),
+        pl.col("EMmr").cast(pl.Float32).alias("ForcedWidth"),
+        pl.lit(-1, dtype=pl.Int64).alias("Supplier loading order"),
+        pl.lit(-1, dtype=pl.Int64).alias("Supplier dock loading order"),
+        pl.lit(-1, dtype=pl.Int64).alias("Plant dock loading order"),
+    ])
+    
+    truck_dims = truck_dims.collect()[df.columns].lazy().unique()
+
+    df = pl.concat([df, truck_dims])
+    df = df.sort(["index", "Length"])
+
+    return df
+
+
+
+
+
+
+
+
+
+
